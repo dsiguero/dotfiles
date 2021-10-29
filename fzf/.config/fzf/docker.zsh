eb9b29b1d3287cbd85528df425d13f8339adcfc5
dsiguero <26867647+dsiguero@users.noreply.github.com>
2021-10-30 00:53:40 +0200

    enable fzf multi selection

--- fzf/.config/fzf/docker.zsh
+++ fzf/.config/fzf/docker.zsh
@@ -1,11 +1,11 @@
 drmi() {
-	docker rmi $(docker images --all | tail -n +2 | fzf --header "Select image to delete" | awk '{print $3}')
+	docker rmi $(docker images --all | tail -n +2 | fzf -m --header "Select image to delete" | awk '{print $3}')
 }
 
 drmc() {
-	docker rm $(docker ps --filter "status=exited" | tail -n +2 | fzf --header "Select container to delete" | awk '{print $1}')
+	docker rm $(docker ps --filter "status=exited" | tail -n +2 | fzf -m --header "Select container to delete" | awk '{print $1}')
 }
 
 dkill() {
-	docker kill $(docker ps | tail -n +2 | fzf --header "Select container to kill" | awk '{print $1}')
+	docker kill $(docker ps | tail -n +2 | fzf -m --header "Select container to kill" | awk '{print $1}')
 }
