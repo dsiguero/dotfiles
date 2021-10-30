ea13ce9822d6a2d597597c9772cbb666f2992d13
dsiguero <26867647+dsiguero@users.noreply.github.com>
2021-10-30 12:52:01 +0200

    add gcloud path and completion

--- zsh/.config/zsh/dev.zsh
+++ zsh/.config/zsh/dev.zsh
@@ -71,4 +71,14 @@ if command -v antigen >/dev/null 2>&1; then
     antigen apply
 fi
 
+# The next line enables shell command completion for gcloud.
+if [ -f $HOME/google-cloud-sdk/completion.zsh.inc ]; then
+    source $HOME/google-cloud-sdk/completion.zsh.inc;
+fi
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f $HOME/google-cloud-sdk/path.zsh.inc ]; then
+    source $HOME/google-cloud-sdk/path.zsh.inc;
+fi
+
 source $XDG_CONFIG/zsh/flexys.zsh
