c69e4ded815f58711a06f3dcaeff2336f4b42829
dsiguero <26867647+dsiguero@users.noreply.github.com>
2021-11-07 12:09:26 +0100

    remove plugins and formatting

--- zsh/.config/zsh/zplug.zsh
+++ zsh/.config/zsh/zplug.zsh
@@ -6,15 +6,7 @@ ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#00617a,bold"		# For solarized dark theme
 
 zplug "plugins/sudo", from:oh-my-zsh					# Prefix current or previous command with sudo by pressing ESC twice.
 zplug "plugins/ssh-agent", from:oh-my-zsh
-
 zplug "MenkeTechnologies/zsh-very-colorful-manuals"		# Coloured man pages
-zplug "jocelynmallon/zshmarks"							# Simple CLI bookmarking plugin: `bookmark 'n`.
-																# 	bookmark '<myname>': creates a new bookmark.
-																#	jump '<myname>': cd to the given bookmark directory.
-
-# Theme and customizations
-# zplug "yardnsm/blox-zsh-theme", as:theme
-zplug romkatv/powerlevel10k, as:theme, depth:1
 
 zplug "plugins/gitfast", from:oh-my-zsh, if:"[[ "${DEV_TOOLS}" == true ]]"
 zplug "plugins/git-extras", from:oh-my-zsh, if:"[[ "${DEV_TOOLS}" == true ]]"
@@ -24,6 +16,7 @@ zplug "plugins/unixorn/git-extra-commands", from:oh-my-zsh, if:"[[ "${DEV_TOOLS}
 zplug "plugins/node", from:oh-my-zsh, if:"[[ "${DEV_MODE_NODE}" == true ]]"
 zplug "plugins/npm", from:oh-my-zsh, if:"[[ "${DEV_MODE_NODE}" == true ]]"
 
+zplug romkatv/powerlevel10k, as:theme, depth:1
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check; then
@@ -35,4 +28,3 @@ fi
 
 # Then, source plugins and add commands to $PATH
 zplug load
-
