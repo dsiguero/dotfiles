45d8a46cc0afbbc2fc764f88cf5dc93e7ce522a3
dsiguero <26867647+dsiguero@users.noreply.github.com>
2021-11-08 21:50:10 +0100

    remove duplicated stuff

--- zsh/.config/zsh/dev.zsh
+++ zsh/.config/zsh/dev.zsh
@@ -73,14 +73,4 @@ if [ -f $HOME/google-cloud-sdk/path.zsh.inc ]; then
     source $HOME/google-cloud-sdk/path.zsh.inc;
 fi
 
-# The next line enables shell command completion for gcloud.
-if [ -f $HOME/google-cloud-sdk/completion.zsh.inc ]; then
-    source $HOME/google-cloud-sdk/completion.zsh.inc;
-fi
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f $HOME/google-cloud-sdk/path.zsh.inc ]; then
-    source $HOME/google-cloud-sdk/path.zsh.inc;
-fi
-
 source $XDG_CONFIG/zsh/flexys.zsh
